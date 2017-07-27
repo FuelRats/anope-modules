41e784ade1166f85476412a24d06c47eb4e10448
Timothy Gunter <tim@vanillaforums.com>
2017-07-27 14:09:37 -0400

    Enhancements to logic, and logging

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # anope-modules
+
+## m_webhooks
+
+This module adds remote webhook support to Anope for certain events.
+
+* ns_register
+* ns_drop
+* ns_group
+
+### Dependencies
+
+`m_webhooks` depends on the following external libraries (and their development headers):
+
+* libcurl4
+
+### Installing
+
+To install m_webhooks:
+
+* Place the `cpp` file inside $anope_source_dir$/modules/third/
+* `cd $anope_source_dir$`
+* ./Config
+* make
+* make install
+* Add the module enabling code to your modules.conf
+
+### Configuration
+
+The following is the default config for this module
+
+```
+module
+{
+	name = "m_webhooks"
+	webhook = ""
+
+	ns_drop = "no"
+	ns_register = "no"
+	ns_group = "no"
+
+	timeout = "5"
+	connect_timeout = "5"
+}
+```
+
+#### webhook
+
+URL to the endpoint that will be called when an event is fired.
+
+Example: *https://www.example.com/hook*
+
+#### ns_drop, ns_register, ns_group
+
+Toggles for events to hook, or not.
+
+#### timeout
+
+How long to wait for a response from the endpoint while calling it.
+
+#### connect_timeout
+
+How long to wait for the socket to connect to the endpoint.
--- modules/m_webhooks/m_webhooks.cpp
+++ modules/m_webhooks/m_webhooks.cpp
@@ -8,31 +8,39 @@
  *
  */
 
+/* RequiredLibraries: curl */
+
 /*
  * Configuration to put into your modules config:
 
 module
 {
 	name = "m_webhooks"
-    url = "https://domain/path"
+	webhook = "https://domain/path"
 
-    ns_drop = "yes"
-    ns_register = "yes"
-    ns_confirm = "yes"
+	ns_drop = "no"
+	ns_register = "no"
+	ns_group = "no"
+
+	timeout = "5"
+	connect_timeout = "5"
 }
 
  */
 
 #include "module.h"
 #include <curl/curl.h>
+#include <iomanip>
+#include <sstream>
 
 class ModuleWebhooks : public Module
 {
-
 	Anope::string webhook_url;
-	bool ns_drop, ns_register, ns_confirm;
+	long timeout, connect_timeout;
+	bool ns_drop, ns_register, ns_confirm, ns_group;
 
 public:
+
 	ModuleWebhooks(const Anope::string &modname, const Anope::string &creator) : Module(modname, creator, EXTRA | VENDOR) {
 		curl_global_init(CURL_GLOBAL_ALL);
 	}
@@ -42,54 +50,75 @@ public:
 	}
 
 	void OnReload(Configuration::Conf *conf) anope_override {
-		webhook_url = Config->GetModule(this)->Get<const Anope::string>("url", "");
-		ns_drop = Config->GetModule(this)->Get<bool>("ns_drop", "no");
-		ns_register = Config->GetModule(this)->Get<bool>("ns_register", "no");
-		ns_confirm = Config->GetModule(this)->Get<bool>("ns_confirm", "no");
+		Log(LOG_DEBUG) << "WEBHOOK config reload";
+		Configuration::Block *block = Config->GetModule(this);
+
+		this->webhook_url = block->Get<const Anope::string>("webhook", "none");
+		Log(LOG_DEBUG) << "WEBHOOK config webhook_url: " << this->webhook_url;
+
+		this->ns_drop = block->Get<bool>("ns_drop", "no");
+		this->ns_register = block->Get<bool>("ns_register", "no");
+		this->ns_group = block->Get<bool>("ns_group", "no");
+		this->timeout = block->Get<long>("timeout", "5");
+		this->connect_timeout = block->Get<long>("connect_timeout", "5");
 	}
 
 	void OnNickDrop(CommandSource &source, NickAlias *na) {
-		if (ns_drop)
-			this->PostCall("ns_drop", this->webhook_url, source.GetUser());
+		if (ns_drop && source.GetUser())
+			PostCall("ns_drop", source.GetUser()->nick.c_str());
 	}
 
 	void OnNickRegister(User *user, NickAlias *na, const Anope::string &pass) {
-		if (ns_register)
-			this->PostCall("ns_register", this->webhook_url, user);
+		if (ns_register && user)
+			PostCall("ns_register", user->nick.c_str());
 	}
 
-	void OnNickConfirm(User *user, NickCore *nc) {
-		if (ns_confirm)
-			this->PostCall("ns_confirm", this->webhook_url, user);
+	void OnNickGroup(User *user, NickAlias *na) {
+		if (ns_group && user)
+			PostCall("ns_group", user->nick.c_str());
 	}
 
 private:
-	void PostCall(const std::string &event, Anope::string &url, User *user) {
-		Log(LOG_DEBUG) << "m_webhooks: posting to " << url << " for '" << event << "'";
+	void PostCall(const std::string &event, const char *user) {
+		Log(LOG_NORMAL) << "WEBHOOK request [" << event << "] sent to " << this->webhook_url;
 
 		CURL *curl = curl_easy_init();
 		if (curl) {
 
 			Anope::string ua = "Anope/" + Anope::Version();
 
-			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-			curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+			curl_easy_setopt(curl, CURLOPT_URL, webhook_url.c_str());
 			curl_easy_setopt(curl, CURLOPT_USERAGENT, ua.c_str());
+
+			curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, connect_timeout);
+			curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+			curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
 			curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
 			curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 0L);
 
 			// Build fields
-			Anope::string data = "event="+event+"&user="+user->nick;
+			Anope::string data = "event="+event+"&user="+user;
 			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
-			curl_easy_perform(curl);
+			CURLcode res = curl_easy_perform(curl);
+
+			if (res > 0) {
+				Log(LOG_NORMAL) << "WEBHOOK error [" << res << "] " << curl_easy_strerror(res);
+			} else {
+				// Check response
+				long response_code;
+				double d_elapsed;
+				curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+				curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &d_elapsed);
+
+				int statuscode = (int) response_code;
+				std::stringstream ss;
+				ss << std::fixed << std::setprecision(2) << d_elapsed;
+				Log(LOG_NORMAL) << "WEBHOOK response [" << statuscode << "] in " << ss.str() << "s";
+			}
+
 			curl_easy_cleanup(curl);
 		}
-	}
-
-	static size_t WriteFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
-		data->append((char*) ptr, size * nmemb);
-		return size * nmemb;
 	}
 };
 
